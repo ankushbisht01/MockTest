1f8a95f9d412321156265445b25c71d24f15a65f
Ankush <ankushbisht72@gmail.com>
2023-05-22 12:38:32 +0530

    fixed deletion error

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("index/", views.homepage, name="homepage"),
     path("test/<int:test_id>/", views.test, name="test"),
     path("getTest/<int:test_id>/", views.getTest, name="getTest"),
-    path("delete/<int:test_id>/", views.delete, name="delete"),
+    path("delete/<int:test_id>/", views.deleteTest, name="delete"),
     path("login/", views.Login, name="login"),
     path("logout/", views.Logout, name="logout"),
 
